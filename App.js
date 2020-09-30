72b533ee16a53dfb87dc6f82266c38a3bae050e0
sentinelbot <ikmazameti@gmail.com>
2020-09-30 17:52:26 +0000

    drawer fixes

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 //COMPONENTS
 import Tab1 from "./src/screens/tabs/Tab1";
@@ -13,14 +14,16 @@ import Tab5 from "./src/screens/tabs/Tab5";
 import Tab6 from "./src/screens/tabs/Tab6";
 import Tab7 from "./src/screens/tabs/Tab7";
 
+import Home from "./src/screens/drawers/Home";
+import Settings from "./src/screens/drawers/Settings";
+import Bookmarks from "./src/screens/drawers/Bookmarks";
+
 const TopTabs = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <View>
-        <Text>News App</Text>
-      </View>
+  function HomeTabs() {
+    return (
       <TopTabs.Navigator>
         <TopTabs.Screen name="Tab1" component={Tab1} />
         <TopTabs.Screen name="Tab2" component={Tab2} />
@@ -30,6 +33,19 @@ export default function App() {
         <TopTabs.Screen name="Tab6" component={Tab6} />
         <TopTabs.Screen name="Tab7" component={Tab7} />
       </TopTabs.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>News App</Text>
+      </View>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeTabs} />
+        <Drawer.Screen name="Settings" component={Settings} />
+        <Drawer.Screen name="Bookmarks" component={Bookmarks} />
+      </Drawer.Navigator>{" "}
     </NavigationContainer>
   );
 }
